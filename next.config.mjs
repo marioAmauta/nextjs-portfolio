bba48aea24133a9cd248be01a8e411b35ebd2e52
mario <sks_894@hotmail.com>
2024-08-14 11:51:37 -0400

    refactor: Update next.config.js to use ES modules syntax

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-const createNextIntlPlugin = require("next-intl/plugin");
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
@@ -11,4 +11,4 @@ const nextConfig = {
   }
 };
 
-module.exports = withNextIntl(nextConfig);
+export default withNextIntl(nextConfig);
