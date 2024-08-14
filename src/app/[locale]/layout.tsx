f45d5cafc0a7eafee6a8455c93c15045a080c04c
mario <sks_894@hotmail.com>
2024-08-14 13:02:54 -0400

    Add vercel analytics and vercel speed insights

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,17 @@
+import { locales } from "@/config";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Metadata } from "next";
-import { ReactNode } from "react";
-import { METADATA_DEFAULT } from "@/lib/constants";
 import { getMessages, getTranslations, unstable_setRequestLocale } from "next-intl/server";
+
 import { IntlClientProvider } from "@/providers/next-intl-provider";
 import { NextThemesProvider } from "@/providers/next-themes-provider";
-import { locales } from "@/config";
-import { Header } from "@/components/header.client";
-import { Footer } from "@/components/footer";
+
+import { METADATA_DEFAULT } from "@/lib/constants";
+
 import { ArrowUpButton } from "@/components/buttons.client";
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header.client";
 
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
@@ -59,7 +63,7 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export default async function LocaleLayout({ children, params: { locale } }: { children: ReactNode; params: { locale: string } }) {
+export default async function LocaleLayout({ children, params: { locale } }: LayoutProps) {
   unstable_setRequestLocale(locale);
 
   const messages = await getMessages();
@@ -70,7 +74,11 @@ export default async function LocaleLayout({ children, params: { locale } }: { c
         <NextThemesProvider>
           <IntlClientProvider locale={locale} messages={messages}>
             <Header />
-            <main className="px-4 lg:px-5% xl:px-10% 2xl:px-15%">{children}</main>
+            <main className="px-4 lg:px-5% xl:px-10% 2xl:px-15%">
+              {children}
+              <Analytics />
+              <SpeedInsights />
+            </main>
             <Footer />
             <ArrowUpButton />
           </IntlClientProvider>
