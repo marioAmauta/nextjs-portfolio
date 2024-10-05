a56c44d0c0f560529b46fcf903483593ff6de047
mario <sks_894@hotmail.com>
2024-10-04 23:25:26 -0300

    Disable eslint no console

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import React from "react";
 import { useEffect } from "react";
 
 import { TypographyH1, TypographyP } from "@/components/ui/typography";
 
 export default function Error({ error }: ErrorFileProps) {
   useEffect(() => {
+    // eslint-disable-next-line no-console
     console.error(error);
   }, [error]);
 
